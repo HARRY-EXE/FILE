e583eacf6243747eade9449ad54bc90c22c22e2d
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-06-01 10:58:52 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,32 +1,48 @@
+#---------[FILE MAKER TOOL BY HARRY]---------#
+#----->    SCRIPTED BY HARRY AKUMA
+#----->    FROM NEPAL NEED STARS
+#----->    LOTS OF LOVE <3
+
+
+#-----> MODULES IMPORT
+
 import platform
 import os
 import requests
+import time
+
+#-----> GLOBAL VALUES
 
 global arc
+
+#-----> CHECKING FOR UPDATES
 
 print(f' •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES ')
 os.system('git pull --quiet')
 
-def check_python_architecture():
+#-----> MAIN DEF
+
+def file_harry():
     global arc
     architecture = platform.architecture()
     if architecture[0] == '32bit':
         arc = "32BIT"
-        print(f' •\x1b[38;5;196m ->\x1b[37m 32BIT DETECTED')
-        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ')
+        print(f' •\x1b[38;5;196m ->\x1b[37m 32BIT DETECTED');time.sleep(1)
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ');time.sleep(1)
         import data.FILE32
         data.FILE32.follow_harry()
         data.FILE32.login()
     elif architecture[0] == '64bit':
         arc = "64BIT"
-        print(f' •\x1b[38;5;196m ->\x1b[37m 64BIT DETECTED')
-        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ')
+        print(f' •\x1b[38;5;196m ->\x1b[37m 64BIT DETECTED');time.sleep(1)
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ');time.sleep(1)
         import data.FILE64
         data.FILE64.follow_harry()
         data.FILE64.login()
     else:
-        arc = "INVALID"
+        exit(f' •\x1b[38;5;196m ->\x1b[37m YOU CANT USE THIS TOOL ');time.sleep(1)
 
+#-----> SYSTEM CONTROL
 
 if __name__ == "__main__":
     check_python_architecture()
